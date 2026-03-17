f42f0e11a6a3068431809ddae03278803bf68939
zoecsmc2024-alt <zoecsmc2024@gmail.com>
2026-03-17 16:54:19 +0300

    Update zoe_app.py

--- zoe_app.py
+++ zoe_app.py
@@ -634,8 +634,7 @@ elif page == "📄 Client Transaction Ledger":
                     "Payment Rec'd": payment_received,
                     "Running Balance": max(0, curr_bal)
                 })
-
-            ledger_final = pd.DataFrame(ledger_entries)
+        ledger_final = pd.DataFrame(ledger_entries)
 
         if not ledger_final.empty:
                 # Summary Metrics
