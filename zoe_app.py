f8c059c371584395cd21e0b19237e7facc9762a8
zoecsmc2024-alt <zoecsmc2024@gmail.com>
2026-03-24 18:29:31 +0300

    Update zoe_app.py

--- zoe_app.py
+++ zoe_app.py
@@ -20,7 +20,7 @@ import io
 def generate_client_pdf(client_name, df):
 
 # Place this right after your imports
-@st.cache_resource
+    @st.cache_resource
 def connect_to_gsheets():
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     creds_dict = st.secrets["gcp_service_account"]
